1d1bab1c7f890b162c79432733b18551d7648198
Utsav4862 <utsavdholiya48@gmail.com>
2023-01-02 01:32:50 +0530

    bug fixed

--- src/API/api.js
+++ src/API/api.js
@@ -1,3 +1,3 @@
-// export const URL =
-//   "https://study-material-backend-production.up.railway.app/api";
-export const URL = "http://localhost:5555/api";
+export const URL =
+  "https://study-material-backend-production.up.railway.app/api";
+// export const URL = "http://localhost:5555/api";
